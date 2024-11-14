7ffc33a14c16f1cfda2fc11b189a2c3995350e47
iglovaci <iulia.glovaci@medisin.uio.no>
2024-11-14 14:12:03 +0100

    Add files via upload

---
+++ Figure 8e_to_m_control/241106a_1200axons_dep0.sh
@@ -0,0 +1,9 @@
+echo 'Run simulation'
+module purge
+module load Python/3.9.6-GCCcore-11.2.0
+
+time srun -A ec12 --time=4-20:0:0 --mem=64G --ntasks=1 --cpus-per-task=32 --ntasks-per-node=1 ~/.local/bin/nrniv FFI_BS_separate_241106a_IPSG0_dep0.hoc &
+time srun -A ec12 --time=4-20:0:0 --mem=64G --ntasks=1 --cpus-per-task=32 --ntasks-per-node=1 ~/.local/bin/nrniv FFI_BS_separate_241106a_IPSG15_dep0.hoc &
+time srun -A ec12 --time=4-20:0:0 --mem=64G --ntasks=1 --cpus-per-task=32 --ntasks-per-node=1 ~/.local/bin/nrniv FFI_BS_separate_241106a_IPSG30_dep0.hoc &
+time srun -A ec12 --time=4-20:0:0 --mem=64G --ntasks=1 --cpus-per-task=32 --ntasks-per-node=1 ~/.local/bin/nrniv FFI_BS_separate_241106a_IPSG75_dep0.hoc &
+time srun -A ec12 --time=4-20:0:0 --mem=64G --ntasks=1 --cpus-per-task=32 --ntasks-per-node=1 ~/.local/bin/nrniv FFI_BS_separate_241106a_IPSG225_dep0.hoc 
---
+++ Figure 8e_to_m_control/loadnrniv.sh
@@ -0,0 +1,3 @@
+module purge
+module load Python/3.9.6-GCCcore-11.2.0
+~/.local/bin/nrniv
